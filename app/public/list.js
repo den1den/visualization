77cb6bebc2d3d84480d6f1788c05c7489bde736b
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-04-09 17:14:13 +0200

    partially added chnage listeners to the list

--- app/public/list.js
+++ app/public/list.js
@@ -19,12 +19,11 @@ function ListSelector() {
     function setList(index) {
         var i = -1;
         while (++i <= 2) {
-            lists[i].style("display", i === index ? "flex" : "none");
-            listButtons[i].attr("class", "nav-link" + (i === index ? " active" : ""));
+            var isShow = i === index || (i === 0 && index === -1);
+            lists[i].style("display", isShow ? "flex" : "none");
+            listButtons[i].attr("class", "nav-link" + (isShow ? " active" : ""));
         }
     }
-
-    this.setList = setList;
 
     function getOnClickFn(d, index) {
         return function () {
@@ -32,24 +31,42 @@ function ListSelector() {
         }
     }
 
-    var fillList = function (features, index) {
-        features.sort(sortByProperty(propertyKey[index])).forEach(function (d) {
-            lists[index].append('li')
-                .attr('class', 'list-group-item')
-                .append('span')
-                .attr('class', 'tag tag-default tag-pill float-xs-right')
-                .text(d.properties[propertyKey[index]])
-                .on('click', getOnClickFn(d, index));
-        });
-    };
-
     this.bindData = function (data) {
         function append(index) {
             var features = data.getFeature(index).features;
-            fillList(features, index);
+            features.sort(sortByProperty(propertyKey[index])).forEach(function (d) {
+                var elName = d.properties[propertyKey[index]],
+                    elIndex = d.properties[propertyCodeKey[index]];
+                lists[index].append('li')
+                    .attr('class', 'list-group-item')
+                    .attr('id', 'list-item-'+index+'-'+elIndex)
+                    .append('span')
+                    .attr('class', 'tag tag-default tag-pill float-xs-right')
+                    .text(elName)
+                    .on('click', getOnClickFn(d, index));
+            });
         }
         append.call(this, 0);
         append.call(this, 1);
         append.call(this, 2);
+
+        data.addChangeListener(function (source, newSelected, newSelectedLevel, oldSelected, oldSelectedLevel) {
+            if(oldSelectedLevel !== -1 && oldSelected !== null) {
+                var oldElIndex = oldSelected.properties[propertyCodeKey[oldSelectedLevel]],
+                    oldEl = lists[oldSelectedLevel].select('#list-item-' + oldSelectedLevel + '-' + oldElIndex);
+                oldEl.classed('active', false);
+                oldEl.select('button').remove();
+            }
+            if(newSelected !== null){
+                var elIndex = newSelected.properties[propertyCodeKey[newSelectedLevel]];
+                var el = lists[newSelectedLevel].select('#list-item-' + newSelectedLevel + '-' + elIndex);
+                el.classed('active', true);
+                el.append('button')
+                    .on('click', function () {
+                        console.log("clicked");
+                    })
+            }
+            setList(newSelectedLevel);
+        })
     };
 }
